1659cd9acc224218af6a494b55260402510c7ac1
root <root@DESKTOP-LMKA10C>
2024-09-04 10:46:38 +0300

    add cron auto, loop emails

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # search_words
+
+
+# to add as a cronjob runs every 10sec
+```bash
+* * * * * for i in {1..6}; do /time_scan/scan_public_html.sh & sleep 10; done
+```
+##    then restart cron service
+      ```bash
+        systemctl restart crond
+      ```
+    
--- search_words.sh
+++ search_words.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 directory="public_html"
+script_name="search_words.sh"
+server_ip=$(curl -s ifconfig.me | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}')
+hostname=$(hostname)
 
 wordlist_path="wordlist.txt"
 mapfile -t words < <(grep -Ev "^$" "$wordlist_path")
 emails_path="emails.txt"
 mapfile -t recipient_email < <(grep -Ev "^$" "$emails_path")
 
+public_dirs=$(find /home/*/public_html -type d -name "$directory" 2> /dev/null)
+temp_file=$(mktemp)
+
 ##########################EXCLUDE##############################
+
 exclusions_path="exclude.txt"
 mapfile -t exclusions < <(grep -Ev "^$" "$exclusions_path")
 exclusion_patterns=""
 for exclusion in "${exclusions[@]}"; do
     exclusion_patterns+=" -not -path $exclusion "
 done
-###############################################################
-
-public_dirs=$(find /home/*/public_html -type d -name "$directory" 2> /dev/null)
-temp_file=$(mktemp)
 
 
 
+
+
+######################## SEARCHING WORDS ######################
 for dir in $public_dirs; do  #Find directories     
-files=$(find "$dir" -depth -type f $exclusion_patterns \
- | awk '{ if ($0 !~ /error_log$/ && $0 !~ /\/debug\/.*\.txt$/) print }')
+files=$(find "$dir" -depth -type f -mmin -0.25 $exclusion_patterns \
+ | awk '{ if ($0 !~ /error_log$/ && $0 !~ /\/debug\/.*\.txt$/ && $0 !~ /\/qr\/.*\.png$/) print }')
   for file in $files; do  #iterate on each element of array 'files' created above
          for word in "${words[@]}"; do
                 if grep -q "$word" "$file"; then
@@ -35,8 +41,18 @@ done
 
 
 if [[ -s "$temp_file" ]]; then
-  mail -s "Word Search Results" "${recipient_email[@]}" < "$temp_file"
+   echo "Script Runtime $elapsed_time" >> $temp_file
+   for email in "${recipient_email[@]}";do
+         mail -s "$script_name report,[$server_ip] [$hostname]" "$email" < "$temp_file" 2> /dev/null
+   done
 fi
 
 #Clean up
 rm -f "$temp_file"
+
+
+# Add to cron
+if ! grep -q "for i in {1..6}; do /search_words/search_words.sh & sleep 10; done" /etc/crontab; then
+  echo "* * * * *    root    for i in {1..6}; do /search_words/search_words.sh & sleep 10; done" >> /etc/crontab
+  systemctl restart crond
+fi
